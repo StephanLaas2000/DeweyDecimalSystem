1fc611c67c8de8bb1dcc67eb47f20551c46f7186
Stephan Laas <stephan2000laas@gmail.com>
2022-10-25 11:21:40 +0200

    Update

--- DeweySystem/Core/worker.cs
+++ DeweySystem/Core/worker.cs
@@ -7,7 +7,7 @@ namespace DeweySystem.Core
 {
     public class worker
     {
-        //test
+       
         public static string filePathDeweyList = "deweyDecimalList.txt";
         public static string filePathProgressBarIdentifyArea = "userProgressBarIdentifyArea.txt";
         public static string fileReplaceBook = "userProgressBar.txt";
